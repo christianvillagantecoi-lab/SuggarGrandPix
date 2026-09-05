3acc966ee9386daf8631a2ee4ffdbed164ecca40
christianvillagantecoi-lab <christian.villagante.coi@pcu.edu.ph>
2026-09-05 17:43:15 +0800

    Update goback.js

--- goback.js
+++ goback.js
@@ -3,9 +3,9 @@
     // Create the button element
     const backButton = document.createElement('a');
     backButton.id = 'modern-back-btn';
-    backButton.href = 'https://www.google.com'; // Change to your destination link or HTML file
+    backButton.href = 'https://christianvillagantecoi-lab.github.io/MyPortfolio/'; // Change to your destination link or HTML file
     backButton.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="https://christianvillagantecoi-lab.github.io/MyPortfolio/" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
             <line x1="19" y1="12" x2="5" y2="12"></line>
             <polyline points="12 19 5 12 12 5"></polyline>
         </svg>
